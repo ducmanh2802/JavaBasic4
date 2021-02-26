181672f7f58636ce38c1f7c7ab24223243a98b6b
Dang Duc Manh <ducmanhtb2802@gmail.com>
2021-02-26 19:35:19 +0700

    Update

--- QuanLySinhVien.java
+++ QuanLySinhVien.java
@@ -2,26 +2,29 @@ public class QuanLySinhVien {
     public static void main(String[] args) throws Exception {
         SinhVien sv1 = new SinhVien();
         sv1.maSV = 20211;
-        sv1.name = "Hung";
-        sv1.diemLT = 6;
-        sv1.diemTH = 7;
-        System.out.println("Điểm trung bình của sinh viên" + sv1.name + " là:" + sv1.tinhDTB());
+        sv1.hoTen = "Hung";
+        sv1.diemLT = 6.0f;
+        sv1.diemTH = 7.0f;
 
         SinhVien sv2 = new SinhVien();
         sv2.maSV = 20211;
-        sv2.name = "Hung";
-        sv2.diemLT = 8;
-        sv2.diemTH = 10;
-        System.out.println("Điểm trung bình của sinh viên" + sv2.name + " là:" + sv2.tinhDTB());
+        sv2.hoTen = "Hung";
+        sv2.diemLT = 8.5f;
+        sv2.diemTH = 10.0f;
+
 
         SinhVien sv3 = new SinhVien();
         sv3.maSV = 20211;
-        sv3.name = "Hung";
-        sv3.diemLT = 8;
-        sv3.diemTH = 7;
-        System.out.println("Điểm trung bình của sinh viên" + sv3.name + " là:" + sv3.tinhDTB());
+        sv3.hoTen = "Hung";
+        sv3.diemLT = 8.0f;
+        sv3.diemTH = 7.0f;
 
-        SinhVien sv4 = new SinhVien(20204, "Minh", 9, 8);
-        System.out.println("Điểm trung bình của sinh viên" + sv4.name + " là:" + sv4.tinhDTB());
+        SinhVien sv4 = new SinhVien(20204, "Minh", 9.0f, 8.5f);
+        System.out.printf("%5s %10s %18s %18s %18s \n", "Ma SV", "Ho Ten", "Diem LT", "Diem TH","Diem trung binh");
+        sv1.display();
+        sv2.display();
+        sv3.display();
+        sv4.display();
+
     }
 }
--- SinhVien.java
+++ SinhVien.java
@@ -1,17 +1,22 @@
+
 public class SinhVien {
     int maSV;
-    String name;
+    String hoTen;
     float diemLT;
     float diemTH;
     public SinhVien(){}
-    public SinhVien(int maSV, String name, float diemLT, float diemTH) {
+    public SinhVien(int maSV, String hoTen, float diemLT, float diemTH) {
         this.maSV = maSV;
-        this.name = name;
+        this.hoTen = hoTen;
         this.diemLT = diemLT;
         this.diemTH = diemTH;
     }
 
     public float tinhDTB() {
         return (diemLT + diemLT) / 2;
+    }
+
+    public void display(){
+        System.out.printf("%3d\t %-15s %8.2f %18.2f %19.2f \n", maSV, hoTen, diemLT, diemTH,tinhDTB());
     }
 }
